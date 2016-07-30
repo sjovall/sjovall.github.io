0c583e8d52928ee3165384991f62a64e30b3f7d2
Andreas Sjövall <a.n.d.r.e.a.s_95@hotmail.com>
2016-07-30 17:31:52 +1200

    Adds cache from root and index to systemworker

--- sw.js
+++ sw.js
@@ -25,7 +25,8 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('airhorner').then(function(cache) {
       return cache.addAll([
-
+        '/',
+        '/index.html',
         '/sounds/airhorn.mp3'
       ]).then(function() {
         return self.skipWaiting();
